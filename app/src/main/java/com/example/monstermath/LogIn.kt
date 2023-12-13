489599b0729bd0f33e3c18887dfd748ba6eef91b
Rory <p2628240@my365.dmu.ac.uk>
2023-12-13 22:02:43 +0000

    layout

---
+++ app/src/main/java/com/example/monstermath/LogIn.kt
@@ -0,0 +1,11 @@
+package com.example.monstermath
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LogIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
