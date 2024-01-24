31483cb751288bde27a94704a61af26e438f8905
Rory <p2628240@my365.dmu.ac.uk>
2024-01-24 15:18:52 +0000

    layout

---
+++ app/src/main/java/com/example/monstermath/Controller/Register.kt
@@ -0,0 +1,66 @@
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.EditText
+import android.widget.Toast
+import com.example.monstermath.Model.Customer
+
+import com.example.monstermath.R
+
+class Register : AppCompatActivity() {
+
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var confirm: EditText
+    private lateinit var email: EditText
+    private lateinit var fullname: EditText
+    private lateinit var db: CustomerDBHelper
+    private lateinit var register: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register)
+
+        //val backbutton: Button = findViewById(R.id.backButton)
+        //backbutton.setOnClickListener {
+         //   val intent = Intent(this, HomePage::class.java)
+            //startActivity(intent)
+      //  }
+
+        username = findViewById(R.id.Username)
+        password = findViewById(R.id.Password)
+        confirm = findViewById(R.id.ConfirmPassword)
+        email = findViewById(R.id.Email)
+        fullname = findViewById(R.id.Name)
+        db = CustomerDBHelper(this)
+        register = findViewById(R.id.RegisterConfirm)
+
+        register.setOnClickListener {
+            val userText = username.text.toString()
+            val passText = password.text.toString()
+            val conText = confirm.text.toString()
+            val emailText = email.text.toString()
+            val fullnameText = fullname.text.toString()
+
+            if (TextUtils.isEmpty(userText) || TextUtils.isEmpty(passText) || TextUtils.isEmpty(emailText) || TextUtils.isEmpty(fullnameText)) {
+                Toast.makeText(this, "Please enter all details", Toast.LENGTH_SHORT).show()
+            } else {
+                if (passText == conText) {
+                    val saveData = db.insertData(userText, passText, emailText, fullnameText)
+
+                    if (saveData) {
+                        Toast.makeText(this, "Sign Up complete", Toast.LENGTH_SHORT).show()
+                        //val intent = Intent(this, LogIn::class.java)
+                        //startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "User already exists", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(this, "Password doesn't match", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/monstermath/Model/CustomerDBHelper.kt
@@ -0,0 +1,80 @@
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.monstermath.Model.Customer
+
+class CustomerDBHelper(context: Context) : SQLiteOpenHelper(context, "Customer", null, 2) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("create table Customer(username TEXT primary key, password TEXT, email TEXT, fullname TEXT)");
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        db?.execSQL("drop table if exists Customer");
+        onCreate(db);
+    }
+
+    fun insertData(
+        username: String,
+        password: String,
+        email: String,
+        fullname: String
+    ): Boolean {
+        val db = this.writableDatabase
+        val cv = ContentValues()
+        cv.put("username", username);
+        cv.put("password", password);
+        cv.put("email", email);
+        cv.put("fullname", fullname);
+        val result = db.insert("Customer", null, cv);
+        db.close()
+        return result != -1L
+    }
+
+    fun checkPass(username: String, password: String): Boolean {
+        val db = this.writableDatabase
+        val query = "select * from Customer where username= '$username' and password= '$password'"
+        val cursor = db.rawQuery(query, null)
+        val result = cursor.count > 0
+        cursor.close()
+        db.close()
+        return result
+    }
+
+    fun getAllCustomers(): List<Customer> {
+        val customerList = mutableListOf<Customer>()
+        val query = "SELECT * FROM Customer"
+        val db = this.readableDatabase
+        val cursor: Cursor? = db.rawQuery(query, null)
+
+        cursor?.use {
+            val usernameIndex = it.getColumnIndex("username")
+            val passwordIndex = it.getColumnIndex("password")
+            val emailIndex = it.getColumnIndex("email")
+            val fullnameIndex = it.getColumnIndex("fullname")
+
+            while (it.moveToNext()) {
+                val username = it.getString(usernameIndex)
+                val password = it.getString(passwordIndex)
+                val email = it.getString(emailIndex)
+                val fullname = it.getString(fullnameIndex)
+                customerList.add(Customer(username, password, email, fullname))
+            }
+        }
+
+        cursor?.close()
+        db.close()
+        return customerList
+    }
+
+    fun removeUser(username: String): Boolean {
+        val db = this.writableDatabase
+        val whereClause = "username = ?"
+        val whereArgs = arrayOf(username)
+        val result = db.delete("Customer", whereClause, whereArgs)
+        db.close()
+        return result > 0
+    }
+}
